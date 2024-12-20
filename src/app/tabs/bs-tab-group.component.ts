1c210b889a413cbfabe62780dedd4b909f0363a8
Albert <krm9346@autuni.ac.nz>
2024-12-20 10:29:23 +0700

    small update

--- src/app/tabs/bs-tab-group.component.ts
+++ src/app/tabs/bs-tab-group.component.ts
@@ -5,28 +5,12 @@ import { CommonModule } from "@angular/common";
 @Component({
     selector: 'app-bs-tab-group',
     imports:[CommonModule],
-    template: `
-    <ul class="nav nav-tabs">
-  <li class="nav-item" *ngFor="let tab of tabPanelList; let idx = index">
-    <a
-      class="nav-link"
-      [class.active]="idx === tabActiveIndex"
-      (click)="tabActiveIndexChange.emit(idx)"
-    >
-      {{ tab.title }}
-      <button class = "btn btn-danger" (click)="removeTabPanel(tab)">x</button>
-    </a>
-  </li>
-</ul>
-
-    <div class = "tab-body" *ngIf="tabPanelList.length; else noTabs">
-        <ng-container *ngTemplateOutlet="tabPanelList[tabActiveIndex].panelBody">
-        </ng-container>
-    </div>
-
-    <ng-template #noTabs>
-    No more tabs.
-  </ng-template>
-    `,
+    templateUrl: './bs-tab-group.component.html',
+    providers:[
+      {
+        provide:TabGroupComponent,
+        useExisting: BsTabGroupComponent,
+      }
+    ]
   })
   export class BsTabGroupComponent extends TabGroupComponent {}
